cba5f2d42f6f59475758bed4ae84e4471a31b79f
Alexandr Zhernoklev <zhe.depotop@gmail.com>
2019-10-03 23:14:42 +0300

    Add func planet

--- first_boot.py
+++ first_boot.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import ephem
 from setting import TOKEN
 logging.basicConfig(format = '%(asctime)s - %(levelname)s  - %(message)s',
                     level=logging.INFO,
@@ -19,6 +20,16 @@ def talk_to_me(bot, update):
                  update.message.text)
     update.message.reply_text(user_text)# отвечает сообщение назад пользователю
 
+def planet(bot, update):
+    logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.first_name, update.message.chat.id,
+                 update.message.text)
+    planet = update.message.text.split()
+    print(planet[1])
+    const = ephem.Mars('03/10/2019')
+    print(ephem.constellation(const))
+    update.message.reply_text(ephem.constellation(const))
+
+
 def main():
     mybot = Updater(TOKEN) # передаем ключ
 
@@ -27,6 +38,7 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('planet', planet))
 
     mybot.start_polling() # бот начинает ломится в телеграмм
     mybot.idle() # будет работать бот пока мы его не остановим
